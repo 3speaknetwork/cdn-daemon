22bd7d2385bbf402e1c473603c52557607b41f9f
vaultec <47548474+vaultec81@users.noreply.github.com>
2021-11-27 08:27:45 -0800

    fix (setup.ts)): detect creds file properly + fix registration

--- scripts/setup.ts
+++ scripts/setup.ts
@@ -12,8 +12,10 @@ import fs from 'fs'
 
 void (async () => {
     if(fs.existsSync('./data/creds-live')) {
-        console.error('Credientials already exist! Assuming setup was successful')
-        return
+        if(fs.existsSync('./data/creds-live/hostname')) {
+            console.error('Credientials already exist! Assuming setup was successful')
+            return
+        }
     }
     console.log('starting gme setup')
     const seedBuf = Crypto.randomBytes(32);
@@ -66,7 +68,9 @@ void (async () => {
         for(let x = 0; x < 6; x++) {
             nginxConf = nginxConf.replace('FILL_DOMAIN', data.name)
         }
-        fs.mkdirSync('./data/creds-live')
+        if(!fs.existsSync('./data/creds-live')) {
+            fs.mkdirSync('./data/creds-live')
+        }
         fs.writeFileSync('./data/creds-live/keys', `seed=${seedBuf.toString('hex')}`)
         fs.writeFileSync('./data/creds-live/hostname', `${data.name}`)
 
