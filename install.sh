2c0d2ed9bd3ca0e36f43743591d10d110c31992e
vaultec <47548474+vaultec81@users.noreply.github.com>
2021-11-27 11:46:47 -0800

    feat: new install.sh options

--- install.sh
+++ install.sh
@@ -6,7 +6,11 @@ git clone https://github.com/3speaknetwork/cdn-daemon
 cd cdn-daemon
 
 docker-compose build
-./init-letsencrypt
+docker exec -i -t spk-cdn-node npx ts-node ./scripts/setup.ts
+chmod +x ./init-letsencrypt.sh
+./init-letsencrypt.sh
+
+docker commit spk-cdn-node_data
+docker-compose down #safety precaution
 
 docker-compose up -d 
-
