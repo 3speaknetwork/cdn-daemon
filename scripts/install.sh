8418c5788c9d2909b37c922b350556f6553cd10f
vaultec <47548474+vaultec81@users.noreply.github.com>
2021-11-27 17:39:37 -0800

    fix (install.sh): yellow colors on end of output

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,8 @@
+
+echo -e "\u001b[33m######## Beginning Install ##########\u001b[0m"
+
+sleep 2;
+
 apt update
 apt install -y git docker docker-compose
 
@@ -8,17 +13,15 @@ cd cdn-daemon
 docker-compose build
 chmod +x ./init-letsencrypt.sh
 
-docker commit spk-cdn-node_data
 docker-compose down #safety precaution
 
 docker-compose up -d 
 
 docker exec -i -t spk-cdn-node npx ts-node ./scripts/setup.ts
+
 ./init-letsencrypt.sh
 
 hostname=`cat './data/creds-live/hostname'`
-
-
 echo -e "\u001b[33m##################################"
 echo -e  "\u001b[33m##### Installation Complete ######"
 echo -e "\u001b[33m########### Thank You ############"
@@ -30,6 +33,6 @@ echo -e "\u001b[0mThe above is the domain name of your node. This is where you c
 
 echo -e "Please make a copy of your keys located in \u001b[35m./data/creds-live/keys\u001b[0m"
 
-echo NOTE: do not execute this script unless you are installing from scratch.
-echo -e "\u001b[31m##################################"
+echo -e "NOTE: do not execute this script unless you are installing from scratch."
+echo -e "\u001b[33m##################################"
 echo -e "\u001b[0m"
