b45deb8ee3b5d2ef5d55218c720785fc65e7ea09
0xumarkhatab <umarkhatabfrl@gmail.com>
2024-02-06 21:27:05 +0500

    Added listening message in api module

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -5,37 +5,34 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { CdnMiddlewareController } from './controllers/cdn-middleware.controller'
 import { AdminController } from './controllers/admin.controller'
 import { CoreService } from '../services/core.service'
-import {
-    FastifyAdapter,
-    NestFastifyApplication,
-  } from '@nestjs/platform-fastify';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify'
 
 export const coreContainer: { self: CoreService } = {} as any
 
 @Module({
-    imports: [],
-    controllers: [CdnMiddlewareController, AdminController],
-    providers: [],
+  imports: [],
+  controllers: [CdnMiddlewareController, AdminController],
+  providers: [],
 })
 class ControllerModule {}
 
-
 export class ApiModule {
-    instance: CoreService
-    public async listen(instance) {
-        this.instance = instance;
-        coreContainer.self = instance;
-        const app2 = await NestFactory.create<NestFastifyApplication>(
-            ControllerModule,
-            new FastifyAdapter()
-        );
-        await app2.listen(18081);
-        const app = await NestFactory.create(ControllerModule)
+  instance: CoreService
+  public async listen(instance) {
+    this.instance = instance
+    coreContainer.self = instance
+    const app2 = await NestFactory.create<NestFastifyApplication>(
+      ControllerModule,
+      new FastifyAdapter(),
+    )
+    await app2.listen(18081)
+    const app = await NestFactory.create(ControllerModule)
 
-        const swaggerconfig = new DocumentBuilder().setTitle('SPK Indexer CDN Daemon').build()
-        const swaggerDocument = SwaggerModule.createDocument(app, swaggerconfig)
-        SwaggerModule.setup('swagger', app, swaggerDocument)
+    const swaggerconfig = new DocumentBuilder().setTitle('SPK Indexer CDN Daemon').build()
+    const swaggerDocument = SwaggerModule.createDocument(app, swaggerconfig)
+    SwaggerModule.setup('swagger', app, swaggerDocument)
 
-        await app.listen(18080)
-    }
+    await app.listen(18080)
+    console.log('listening on Port 18080')
+  }
 }
