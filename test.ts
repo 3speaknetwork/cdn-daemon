ca6127d081cb3f4e6c37b9610fdda75ec08406a2
0xumarkhatab <umarkhatabfrl@gmail.com>
2024-02-12 15:55:56 +0500

    Added 720p and 1080p variants in tests

--- test.ts
+++ test.ts
@@ -2,10 +2,7 @@ import axios from 'axios'
 
 let baseUri = 'http://localhost:18081/video/resizer/'
 
-export async function estimateTransformation(
-  url: string,
-  videoTimeInSeconds: number,
-): Promise<string> {
+export async function estimateTransformation(url: string): Promise<number> {
   try {
     console.log('Uploading Video....')
 
@@ -14,83 +11,182 @@ export async function estimateTransformation(
 
     const response = await axios.get(baseUri + base64EncodedUrl + '?format=webm&cacheControl=cache')
     let time2 = Date.now() / 1000
-    console.log('Video Uploaded !')
+    console.log('Video Uploaded in ' + (time2 - time1).toString(), ' seconds')
 
-    let stats =
-      ' videoTime : ' +
-      videoTimeInSeconds.toString() +
-      'seconds' +
-      ' responseTime : ' +
-      (time2 - time1).toString() +
-      ' seconds'
-    console.log(stats)
-
-    return stats
+    return time2 - time1
   } catch (error) {
     console.error('Error checking HLS file:', error)
-    return ''
+    return 0
   }
 }
 
 async function runTests() {
   let responses = []
-  let url, videoTime, response
+  let responseTimes = []
+  let url_480p, url_720p, url_1080p, videoTime, responseTime, uploadTimes
 
-  /**                           Test 1                 */
-  url = 'https://threespeakvideo.b-cdn.net/imynodlu/480p.m3u8'
-  videoTime = 16 // seconds
-  response = await estimateTransformation(url, videoTime)
-  responses.push(response)
-
-  /**                           Test 2                  */
-  url =
+  /** Test 1 */
+  url_480p =
     'https://ipfs-3speak.b-cdn.net/ipfs/QmWVqFM6i16zzBjDW6EZbPEoLLjHeTUiimyY9bXjHVCpzE/480p/index.m3u8'
+  url_720p =
+    'https://ipfs-3speak.b-cdn.net/ipfs/QmWVqFM6i16zzBjDW6EZbPEoLLjHeTUiimyY9bXjHVCpzE/720p/index.m3u8'
+  url_1080p =
+    'https://ipfs-3speak.b-cdn.net/ipfs/QmWVqFM6i16zzBjDW6EZbPEoLLjHeTUiimyY9bXjHVCpzE/1080p/index.m3u8'
   videoTime = 27 // seconds
-  response = await estimateTransformation(url, videoTime)
-  responses.push(response)
 
-  /**                           Test 3                  */
-  url =
+  uploadTimes = {
+    videoTime: videoTime,
+  }
+
+  responseTime = await estimateTransformation(url_480p)
+  uploadTimes['480p'] = responseTime + ' seconds'
+  responseTime = await estimateTransformation(url_720p)
+  uploadTimes['720p'] = responseTime + ' seconds'
+  responseTime = await estimateTransformation(url_1080p)
+  uploadTimes['1080p'] = responseTime + ' seconds'
+
+  console.log(uploadTimes)
+  responseTimes.push(uploadTimes)
+
+  /** Test 2 */
+  url_480p =
     'https://ipfs-3speak.b-cdn.net/ipfs/QmSDDkZ9k2U4i64Pnxkg2SDBExh4V39WMiievVV5nuVZRJ/480p/index.m3u8'
-  videoTime = 170 // seconds , 2:50 Minutes
-  response = await estimateTransformation(url, videoTime)
-  responses.push(response)
+  url_720p =
+    'https://ipfs-3speak.b-cdn.net/ipfs/QmSDDkZ9k2U4i64Pnxkg2SDBExh4V39WMiievVV5nuVZRJ/720p/index.m3u8'
+  url_1080p =
+    'https://ipfs-3speak.b-cdn.net/ipfs/QmSDDkZ9k2U4i64Pnxkg2SDBExh4V39WMiievVV5nuVZRJ/1080p/index.m3u8'
+  videoTime = 170 // seconds
 
-  /**                           Test 4                  */
-  url =
+  uploadTimes = {
+    videoTime: videoTime,
+  }
+
+  responseTime = await estimateTransformation(url_480p)
+  uploadTimes['480p'] = responseTime + ' seconds'
+  responseTime = await estimateTransformation(url_720p)
+  uploadTimes['720p'] = responseTime + ' seconds'
+  responseTime = await estimateTransformation(url_1080p)
+  uploadTimes['1080p'] = responseTime + ' seconds'
+
+  console.log(uploadTimes)
+  responseTimes.push(uploadTimes)
+
+  /** Test 3 */
+  url_480p =
     'https://ipfs-3speak.b-cdn.net/ipfs/QmYnYKrxFZif6xBfCDJv87iqenHsb7MXBUefqzpypeBbzk/480p/index.m3u8'
-  videoTime = 292 // seconds , 4:52 Minutes
-  response = await estimateTransformation(url, videoTime)
-  responses.push(response)
+  url_720p =
+    'https://ipfs-3speak.b-cdn.net/ipfs/QmYnYKrxFZif6xBfCDJv87iqenHsb7MXBUefqzpypeBbzk/720p/index.m3u8'
+  url_1080p =
+    'https://ipfs-3speak.b-cdn.net/ipfs/QmYnYKrxFZif6xBfCDJv87iqenHsb7MXBUefqzpypeBbzk/1080p/index.m3u8'
+  videoTime = 292 // seconds
 
-  /**                           Test 5                  */
-  url =
+  uploadTimes = {
+    videoTime: videoTime,
+  }
+
+  responseTime = await estimateTransformation(url_480p)
+  uploadTimes['480p'] = responseTime + ' seconds'
+  responseTime = await estimateTransformation(url_720p)
+  uploadTimes['720p'] = responseTime + ' seconds'
+  responseTime = await estimateTransformation(url_1080p)
+  uploadTimes['1080p'] = responseTime + ' seconds'
+
+  console.log(uploadTimes)
+  responseTimes.push(uploadTimes)
+
+  /** Test 4 */
+  url_480p =
     'https://ipfs-3speak.b-cdn.net/ipfs/QmVuxWHF9aGJUHxLaYuLif2EwWZmVHgWA9c434aFoRQghm/480p/index.m3u8'
-  videoTime = 385 // seconds , 6:25 Minutes
-  response = await estimateTransformation(url, videoTime)
-  responses.push(response)
+  url_720p =
+    'https://ipfs-3speak.b-cdn.net/ipfs/QmVuxWHF9aGJUHxLaYuLif2EwWZmVHgWA9c434aFoRQghm/720p/index.m3u8'
+  url_1080p =
+    'https://ipfs-3speak.b-cdn.net/ipfs/QmVuxWHF9aGJUHxLaYuLif2EwWZmVHgWA9c434aFoRQghm/1080p/index.m3u8'
+  videoTime = 385 // seconds
 
-  /**                           Test 6                  */
-  url =
+  uploadTimes = {
+    videoTime: videoTime,
+  }
+
+  responseTime = await estimateTransformation(url_480p)
+  uploadTimes['480p'] = responseTime + ' seconds'
+  responseTime = await estimateTransformation(url_720p)
+  uploadTimes['720p'] = responseTime + ' seconds'
+  responseTime = await estimateTransformation(url_1080p)
+  uploadTimes['1080p'] = responseTime + ' seconds'
+
+  console.log(uploadTimes)
+  responseTimes.push(uploadTimes)
+
+  /** Test 5 */
+  url_480p =
     'https://ipfs-3speak.b-cdn.net/ipfs/QmQvdUBwiEzxMMmx7mnKHfkscgoBDNyRxGFQNEouZ25meU/480p/index.m3u8'
-  videoTime = 471 // seconds , 7:51 Minutes
-  response = await estimateTransformation(url, videoTime)
-  responses.push(response)
+  url_720p =
+    'https://ipfs-3speak.b-cdn.net/ipfs/QmQvdUBwiEzxMMmx7mnKHfkscgoBDNyRxGFQNEouZ25meU/720p/index.m3u8'
+  url_1080p =
+    'https://ipfs-3speak.b-cdn.net/ipfs/QmQvdUBwiEzxMMmx7mnKHfkscgoBDNyRxGFQNEouZ25meU/1080p/index.m3u8'
+  videoTime = 471 // seconds
 
-  /**                           Test 7                  */
-  url =
+  uploadTimes = {
+    videoTime: videoTime,
+  }
+
+  responseTime = await estimateTransformation(url_480p)
+  uploadTimes['480p'] = responseTime + ' seconds'
+  responseTime = await estimateTransformation(url_720p)
+  uploadTimes['720p'] = responseTime + ' seconds'
+  responseTime = await estimateTransformation(url_1080p)
+  uploadTimes['1080p'] = responseTime + ' seconds'
+
+  console.log(uploadTimes)
+  responseTimes.push(uploadTimes)
+
+  /** Test 6 */
+  url_480p =
     'https://ipfs-3speak.b-cdn.net/ipfs/QmcKneLto83zX87nZmJr3rztc3wgYL3hSDbjhQ2x2nPxbT/480p/index.m3u8'
-  videoTime = 721 // seconds , 12:01 Minutes
-  response = await estimateTransformation(url, videoTime)
-  responses.push(response)
+  url_720p =
+    'https://ipfs-3speak.b-cdn.net/ipfs/QmcKneLto83zX87nZmJr3rztc3wgYL3hSDbjhQ2x2nPxbT/720p/index.m3u8'
+  url_1080p =
+    'https://ipfs-3speak.b-cdn.net/ipfs/QmcKneLto83zX87nZmJr3rztc3wgYL3hSDbjhQ2x2nPxbT/1080p/index.m3u8'
+  videoTime = 721 // seconds
 
-  /**                           Test 8                  */
-  url =
+  uploadTimes = {
+    videoTime: videoTime,
+  }
+
+  responseTime = await estimateTransformation(url_480p)
+  uploadTimes['480p'] = responseTime + ' seconds'
+  responseTime = await estimateTransformation(url_720p)
+  uploadTimes['720p'] = responseTime + ' seconds'
+  responseTime = await estimateTransformation(url_1080p)
+  uploadTimes['1080p'] = responseTime + ' seconds'
+
+  console.log(uploadTimes)
+  responseTimes.push(uploadTimes)
+
+  /** Test 7 */
+  url_480p =
     'https://ipfs-3speak.b-cdn.net/ipfs/QmcKwehxaYBThjBjajCXFDhdaSQ7Kfzb1se4F5T6HwDEY2/480p/index.m3u8'
-  videoTime = 1091 // seconds , 18:11 Minutes
-  response = await estimateTransformation(url, videoTime)
-  responses.push(response)
+  url_720p =
+    'https://ipfs-3speak.b-cdn.net/ipfs/QmcKwehxaYBThjBjajCXFDhdaSQ7Kfzb1se4F5T6HwDEY2/720p/index.m3u8'
+  url_1080p =
+    'https://ipfs-3speak.b-cdn.net/ipfs/QmcKwehxaYBThjBjajCXFDhdaSQ7Kfzb1se4F5T6HwDEY2/1080p/index.m3u8'
+  videoTime = 1091 // seconds
 
-  console.log(responses)
+  uploadTimes = {
+    videoTime: videoTime,
+  }
+
+  responseTime = await estimateTransformation(url_480p)
+  uploadTimes['480p'] = responseTime + ' seconds'
+  responseTime = await estimateTransformation(url_720p)
+  uploadTimes['720p'] = responseTime + ' seconds'
+  responseTime = await estimateTransformation(url_1080p)
+  uploadTimes['1080p'] = responseTime + ' seconds'
+
+  console.log(uploadTimes)
+  responseTimes.push(uploadTimes)
+
+  // Log the response times
+  console.log(responseTimes)
 }
 runTests()
